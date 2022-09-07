97dbf181cbb602aed84e273fc99ec647e4a2c572
dingwe <19157713+ponsapont@users.noreply.github.com>
2022-09-07 18:47:02 +0200

    Initial commit

---
+++ server/src/main.rs
@@ -0,0 +1,33 @@
+use actix_web::{get, post, HttpResponse, Responder};
+use sqlx::postgres::PgPoolOptions;
+
+#[actix_web::main]
+async fn main() -> Result<(), sqlx::Error> {
+    // Create a connection pool
+    //  for MySQL, use MySqlPoolOptions::new()
+    //  for SQLite, use SqlitePoolOptions::new()
+    //  etc.
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:password@localhost/test").await?;
+
+    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool).await?;
+
+    assert_eq!(row.0, 150);
+
+    Ok(())
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
